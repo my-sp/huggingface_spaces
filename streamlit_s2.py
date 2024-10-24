0fd744e7abb5fa028292d3e0fc7aa50eae7e54b2
my-sp <madhurigottumukkala8@gmail.com>
2024-10-24 10:57:57 +0530

    Update streamlit_s2.py

--- streamlit_s2.py
+++ streamlit_s2.py
@@ -1,45 +1,31 @@
 import streamlit as st
-import requests
-from huggingface_hub import InferenceClient
+import transformers
+import torch
 
-st.title("Streamlit with Hugging Face API")
+HF_TOKEN=st.secrets["HF_Token"]
+# Load the model and pipeline
+model_id = "meta-llama/Llama-3.1-8B-Instruct"
 
-def get_models():
-     return ["meta-llama/Meta-Llama-3-8B-Instruct","meta-llama/Llama-3.1-8B-Instruct"]
+# Set up the pipeline with the Hugging Face token
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model_id,
+    model_kwargs={"torch_dtype": torch.bfloat16, "use_auth_token": HF_TOKEN}
+)
 
-models = get_models()
-model_names = get_models()
-placeholder_option = "Select a model"
-model_names_with_placeholder = [placeholder_option] + model_names
-selected_model = st.selectbox("Select the model:", model_names_with_placeholder)
-prompt1 = st.chat_input("Message")
+# Streamlit user interface
+st.title("LLM Model Inference")
+input_text = st.text_input("Enter your prompt:")
 
-if selected_model != placeholder_option:
-    st.write(f"You selected: {selected_model}")
-    api_token = "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"  
-    client = InferenceClient(model=selected_model, token=api_token)
-
-    # Initialize chat history in session state if it doesn't exist
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-
-    # Display chat messages from history on app rerun
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-    if prompt1:  
-        with st.chat_message("user"):
-            st.markdown(prompt1)
-        st.session_state.messages.append({"role": "user", "content": prompt1})
-        try:
-            response = client.text_generation(prompt1)
-        except Exception as e:
-            response = f"An error occurred: {e}"
-        with st.chat_message("assistant"):
-            st.markdown(response)
-    # Add assistant response to chat history
-        st.session_state.messages.append({"role": "assistant", "content": response})
-
-else:
-    st.write("Please select a model.")
+if st.button("Generate"):
+    if input_text:  # Check if the input is not empty
+        # Generate text using the pipeline
+        messages = [
+        {"role": "system", "content": "You are a question answering assistant."},
+        {"role": "user", "content": input_text}
+        ]
+        response = pipeline(messages, max_new_tokens=30)  
+        st.write("Generated Response:")
+        st.write(response[0]['generated_text'][-1]['content'])  
+    else:
+        st.error("Please enter a prompt to generate text.")
